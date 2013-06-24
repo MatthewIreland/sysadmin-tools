d413618a9a46e7049ca0142f12c57624813f68e1
Matthew Ireland <matthew.t.ireland@gmail.com>
2013-06-24 11:52:27 +0100

    Created file for configuring git to my preferred options when first using on a new system.
    As run, it will set standard parameters:
    * user.name "Matthew Ireland"
    * user.email "matthew.t.ireland@gmail.com"
    * cache github username/password for 100000 seconds
    * core.editor emacs
    * help.autocorrect 1
    * color.ui true
    
    In addition, there are various commented options for
    * commit.template
    * core.pager
    * user.signingkey
    * core.excludesfile
    * core.autocrlf
    * color.*
    
    Uncommenting any of these options will set them to a reasonable default. Read the comments/git documentation for more information on exactly how they should be configured.

---
+++ gitconfig.sh
@@ -0,0 +1,30 @@
+git config --global user.name "Matthew Ireland"              # writes to /.gitconfig, rather than .git/config.
+                                                             # alternatively use --system to write to $(prefix)/etc/gitconfig
+git config --global user.email "matthew.t.ireland@gmail.com"
+
+git config credential.helper 'cache'                         # avoid having to type in credentials for github every push/pull
+git config credential.helper 'cache --timeout=100000'        # max value for timeout (seconds)
+
+git config --global core.editor emacs
+
+git config -- global help.autocorrect 1                      # if set to 1, git will automatically run the command if it has only one match (e.g. git com -> git commit)
+
+git config --global color.ui true                            # turns on default colouring if output is to terminal
+                                                             # (set to always if want regardless of the output - equiv to using --color flag)
+
+
+
+# Uncomment below options for them to take effect
+# git config --global comit.template $HOME/.gitmessage.txt     # sets the default message that appears in the editor upon commit
+# git config --global core.pager ''                            # by default, the pager is less. Can change to more (or something else) or just leave blank to turn it off
+# git config --global user.signingkey <gpg-key-id>             # useful when making signed annotated tags
+# git config --global core.excludesfile ''                     # if .gitignore is outside the project directory
+# git config --global core.autocrlf true                       # set to true if working with people on Windows (autoconverts CRLF line endings into LF on commit and vv)
+                                                               # set to false if windows programmer on windows-only project
+# git config --global core.autocrlf input                      # converts on commit, but not the other way round (i.e. fix screwup)
+# git config --global color.branch "blue bold"                 # various colour settings (with example values)
+# git config --global color.diff "blue bold"
+# git config --global color.diff.meta "blue black bold"        # blue foreground, black background, bold text
+# git config --global color.interactive "..."
+# git config --global color.status "..."
+
